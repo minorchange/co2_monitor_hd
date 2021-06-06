9d91d3dcc8363c547a9040d9ea2c97d05cc82502
MeHaniker1 <michi.halfmann@gmail.com>
2021-06-06 22:06:51 +0200

    Update colors.py

--- colors.py
+++ colors.py
@@ -24,7 +24,6 @@ trend_color = _default_hyperlink_color
 
 # barcolors = ["#cad2c5", "#84a98c", "#52796f", "#354f52", "#2f3e46"]
 # barcolors = ["#285d70", "#00b9a0", "#00f4d7", "#b4aa99", "#4e4637"]
-# ---- barcolors = ["#cb997e", "#ddbea9", "#b7b7a4", "#a5a58d", "#6b705c"]
-barcolors = ["#cb0000", "#ddbea9", "#b7b7a4", "#a5a58d", "#6b705c"]
+barcolors = ["#cb997e", "#ddbea9", "#b7b7a4", "#a5a58d", "#6b705c"]
 # barcolors = ["#a37d25", "#31250b", "#000000", "#594414", "#7c5f1c"]
 # barcolors = ["#506050", "#202010", "#040404", "#303040", "#504050"]

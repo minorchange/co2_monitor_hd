92012d86c4247df52a36c99797075b3ce901fc9e
minorchange <bnu@posteo.org>
2024-04-04 22:00:05 +0200

    Minor Change of wording on landing page

--- components/container/container_tldr.py
+++ components/container/container_tldr.py
@@ -21,7 +21,7 @@ def create_container_tldr(app):
                                 "Heidelberg trägt dazu bei, dass Deutschland seine völkerrechtlich verbindlichen Klimaziele verfehlt."
                             ),
                             html.H4(
-                                "Gegen Ende dieses Jahres wird ein Punkt erreicht, an dem selbst bei einer vollständigen Einstellung aller CO₂-Emissionen das Ziel, die globale Erwärmung auf 1,5 Grad Celsius zu begrenzen, wahrscheinlich unerreichbar sein wird. Diese Entwicklung unterstreicht die Unzulänglichkeit bisheriger Klimapolitik. Es ist absehbar, dass mit einer Wahrscheinlichkeit von über 50% auch das langfristige Ziel einer maximalen Erwärmung um 2 Grad Celsius verfehlt wird, sofern die bisherigen Klimaschutzpläne nicht signifikant ambitionierter gestaltet werden."
+                                "Gegen Ende dieses Jahres wird ein Punkt erreicht, an dem selbst bei einer vollständigen Einstellung aller CO₂-Emissionen das Ziel, die globale Erwärmung auf 1,5 Grad Celsius zu begrenzen, mit einer Wahrscheinlichkeit  von mehr als 50% unerreichbar sein wird. Diese Entwicklung unterstreicht die Unzulänglichkeit bisheriger Klimapolitik. Es ist absehbar, dass mit einer Wahrscheinlichkeit von über 50% auch das langfristige Ziel einer maximalen Erwärmung um 2 Grad Celsius verfehlt wird, sofern die bisherigen Klimaschutzpläne nicht signifikant ambitionierter gestaltet werden."
                             ),
                         ],
                         style={
